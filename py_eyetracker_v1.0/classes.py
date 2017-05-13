c8203b2c8f607d9dac52c4e98b707c8aa8be116b
michele-mada <michele.mm1234@gmail.com>
2017-05-13 17:46:26 +0200

    added eye corner detection

---
+++ py_eyetracker_v1.0/classes.py
@@ -0,0 +1,48 @@
+from collections import namedtuple
+
+
+Rect = namedtuple("Rect", ["x", "y", "width", "height"])
+Point = namedtuple("Point", ["x", "y"])
+
+
+class Eye:
+
+    def __init__(self, area, is_right):
+        self.area = Rect(*area)
+        self.is_right = is_right
+        self.pupil_relative = Point(0,0)
+        self.inner_corner_relative = Point(0,0)
+        self.outer_corner_relative = Point(0, 0)
+
+    @property
+    def pupil(self):
+        return Point(self.area.x + self.pupil_relative.x, self.area.y + self.pupil_relative.y)
+
+    @property
+    def inner_corner(self):
+        return Point(self.area.x + self.inner_corner_relative.x, self.area.y + self.inner_corner_relative.y)
+
+    @property
+    def outer_corner(self):
+        return Point(self.area.x + self.outer_corner_relative.x, self.area.y + self.outer_corner_relative.y)
+
+    def set_leftmost_corner(self, point):
+        if self.is_right:
+            self.outer_corner_relative = point
+        else:
+            self.inner_corner_relative = point
+
+    def set_rightmost_corner(self, point):
+        if self.is_right:
+            self.inner_corner_relative = point
+        else:
+            self.outer_corner_relative = point
+
+    def __str__(self):
+        return "Eye(%s, area=%s, pupil=%s, inner_corner=%s, outer_corner=%s" % (
+            "Right" if self.is_right else "Left",
+            str(self.area),
+            str(self.pupil),
+            str(self.inner_corner),
+            str(self.outer_corner),
+        )
--- py_eyetracker_v1.0/main.py
+++ py_eyetracker_v1.0/main.py
@@ -10,28 +10,32 @@ import numpy as np
 import argparse
 
 from utils.camera import WebcamVideoStream
+from classes import Eye, Point
 
 
 camera_port = 0
 
 
-def find_eye_center_and_corners(eye):
 
-    thresh = threshold_otsu(eye) * 0.5
-    eye_binary = eye < thresh
+def find_eye_center_and_corners(eye_image, eye_object, debug=False):
+    assert(isinstance(eye_object, Eye))
 
-    corners = corner_peaks(corner_harris(eye_binary))
+    # Part 1: finding the center of the eye
+
+    # apply a threshold to the image
+    thresh = threshold_otsu(eye_image) * 0.5
+    eye_binary = eye_image < thresh
+
+    # split the thresholded image into regions
+    blobs_labels, nlabels = measure.label(eye_binary, background=0, return_num=True)
 
     centers = []
     accums = []
     radii = []
     hough_radii = np.arange(10, 25, 2)
 
-
-    blobs_labels, nlabels = measure.label(eye_binary, background=0, return_num=True)
-    print("labels:",nlabels)
-
-    for n in range(1, nlabels):  # skip label zero
+    # foreach region, use the hough transform to find the most prominent circle
+    for n in range(1, nlabels):
         img = blobs_labels == n
         hough_res = hough_circle(img, hough_radii)
         _accums, cx, cy, _radii = hough_circle_peaks(hough_res, hough_radii, total_num_peaks=1)
@@ -39,22 +43,54 @@ def find_eye_center_and_corners(eye):
         centers.extend(zip(cx, cy))
         radii.extend(_radii)
 
-    print(accums)
+    # the center of the pupil is the center of the best circular region found
     best_circle_index = np.argmax(np.array(accums))
-    print(best_circle_index)
-    center_y, center_x = centers[best_circle_index]
+    center_x, center_y = centers[best_circle_index]
 
-    #TODO: find eye corners
+    # Part 1: finding the corners of the eye
 
-    fig, ax = plt.subplots(1)
+    # heuristic to find the best corner, given its position relative to the pupil center
+    def corner_measure(corner, center, leftness=1):
+        cy, cx = corner
+        zx, zy = center
+        return (cx-zx) * leftness - 0.5 * abs(cy-zy)
 
-    ax.imshow(eye_binary, cmap="gray")
+    # find the centers (y,x) of the corners, using harris
+    corners = corner_peaks(corner_harris(eye_binary))
 
-    ax.imshow(blobs_labels, cmap='spectral')
-    ax.plot(center_y, center_x, "w+")
-    ax.plot(corners[:,1], corners[:,0], "ro")
+    # give each corner a score, according to our heuristic
+    left_corner_score = map(lambda corner: (corner,
+                                            corner_measure(corner, (center_x, center_y), leftness=1)
+                                            ),
+                            corners)
+    # find the best corner
+    left_corner = max(left_corner_score, key=lambda cs: cs[1])[0]
 
-    return center_y, center_x  #TODO: return eye corners
+    # give each corner a score, according to our heuristic
+    right_corner_score = map(lambda corner: (corner,
+                                             corner_measure(corner, (center_x, center_y), leftness=-1)
+                                             ),
+                             corners)
+    # find the best corner
+    right_corner = max(right_corner_score, key=lambda cs: cs[1])[0]
+
+    if debug:
+        fig, ax = plt.subplots(1)
+
+        ax.imshow(eye_binary, cmap="gray")
+
+        ax.imshow(blobs_labels, cmap='spectral')
+        ax.plot(center_x, center_y, "w+")
+
+        ax.plot(corners[:,1], corners[:,0], "bo")
+
+        ax.plot(left_corner[1], left_corner[0], "ro")
+        ax.plot(right_corner[1], right_corner[0], "go")
+
+    eye_object.pupil_relative = Point(center_x, center_y)
+    eye_object.set_leftmost_corner(Point(x=left_corner[1], y=left_corner[0]))
+    eye_object.set_rightmost_corner(Point(x=right_corner[1], y=right_corner[0]))
+
 
 
 def find_eyes(image, cascPath):
@@ -93,7 +129,7 @@ def split_eyes(eyes):
     Select right eye and left eyes.
     The criteria is simple: the right eye is always the one with smaller x
     :param eyes: list of rectangles representing the eyes
-    :return: tuple (right_eye, left_eye) of rectangles
+    :return: tuple (right_eye, left_eye) of Eye class
     """
     right_eye = eyes[0]
     left_eye = eyes[1]
@@ -101,26 +137,25 @@ def split_eyes(eyes):
     if right_eye[0] > left_eye[0]:
         right_eye, left_eye = left_eye, right_eye
 
-    return right_eye, left_eye
+    return Eye(right_eye, True), Eye(left_eye, False)
 
 
-def process_frame(image_cv2format):
+def process_frame(image_cv2format, debug=False):
     picture_float = img_as_float(image_cv2format)
     picture = exposure.equalize_hist(picture_float)
     eyes = find_eyes(image_cv2format, cli.cascade_file)
     if len(eyes) < 2:
-        return picture, (0, 0, 0, 0), (0, 0, 0, 0), (0, 0), (0, 0)
+        return picture, None, None
+
     right_eye, left_eye = split_eyes(eyes)
 
-    right_eyepatch = cropped_rect(picture, right_eye)
-    right_pupil_x, right_pupil_y = find_eye_center_and_corners(right_eyepatch)
-    right_pupil = (right_pupil_x + right_eye[0], right_pupil_y + right_eye[1])
+    right_eyepatch = cropped_rect(picture, right_eye.area)
+    find_eye_center_and_corners(right_eyepatch, right_eye, debug=debug)
 
-    left_eyepatch = cropped_rect(picture, left_eye)
-    left_pupil_x, left_pupil_y = find_eye_center_and_corners(left_eyepatch)
-    left_pupil = (left_pupil_x + left_eye[0], left_pupil_y + left_eye[1])
+    left_eyepatch = cropped_rect(picture, left_eye.area)
+    find_eye_center_and_corners(left_eyepatch, left_eye, debug=debug)
 
-    return picture, right_eye, left_eye, right_pupil, left_pupil
+    return picture, right_eye, left_eye
 
 
 def one_shot(cli):
@@ -129,34 +164,43 @@ def one_shot(cli):
     #(channel_h, channel_s, channel_v) = cv2.split(image_hsv)
     image_cv2_gray = cv2.cvtColor(image_cv2, cv2.COLOR_RGB2GRAY)
 
-    picture, right_eye, left_eye, right_pupil, left_pupil = process_frame(image_cv2_gray)
+    picture, right_eye, left_eye  = process_frame(image_cv2_gray, debug=cli.debug)
 
-    print("Eyes (R,L): %s, %s\n  Right pupil: %s\n  Left pupil: %s" %
-          (str(right_eye), str(left_eye), str(right_pupil), str(left_pupil)))
+    print("Eyes (R,L):\n  %s,\n  %s" % (str(right_eye), str(left_eye)))
 
     # draw stuff
-    eye1 = right_eye
-    eye2 = left_eye
     fig, ax = plt.subplots(1)
+
     ax.imshow(picture, cmap="gray")
+
+    # draw eye regions (found using the haar cascade)
     ax.add_patch(patches.Rectangle(
-        (eye1[0], eye1[1]),  # (x,y)
-        eye1[2],  # width
-        eye1[3],  # height
+        (right_eye.area.x, right_eye.area.y),
+        right_eye.area.width,
+        right_eye.area.height,
         linewidth=1, edgecolor='r', facecolor='none'
     ))
     ax.add_patch(patches.Rectangle(
-        (eye2[0], eye2[1]),  # (x,y)
-        eye2[2],  # width
-        eye2[3],  # height
+        (left_eye.area.x, left_eye.area.y),
+        left_eye.area.width,
+        left_eye.area.height,
         linewidth=1, edgecolor='r', facecolor='none'
     ))
-    ax.plot(right_pupil[0], right_pupil[1], "r+")
-    ax.plot(left_pupil[0], left_pupil[1], "r+")
+    # draw pupil spots (red)
+    ax.plot(right_eye.pupil[0], right_eye.pupil[1], "r+")
+    ax.plot(left_eye.pupil[0], left_eye.pupil[1], "r+")
+    # draw inner corners (green)
+    ax.plot(right_eye.inner_corner[0], right_eye.inner_corner[1], "g+")
+    ax.plot(left_eye.inner_corner[0], left_eye.inner_corner[1], "g+")
+    # draw outer corners (blue)
+    ax.plot(right_eye.outer_corner[0], right_eye.outer_corner[1], "b+")
+    ax.plot(left_eye.outer_corner[0], left_eye.outer_corner[1], "b+")
+
     plt.show()
 
 
 def live(cli):
+    #TODO: make this function work again
     camera = WebcamVideoStream(src=camera_port)
     camera.start()
     plt.ion()
@@ -204,6 +248,7 @@ def parsecli():
     parser.add_argument('file', help='filename of the picture; - for webcam', type=str)
     parser.add_argument('-c', '--cascade-file', help='path to the .xml file with the eye-detection haar cascade',
                         type=str, default="haarcascade_righteye_2splits.xml")
+    parser.add_argument('-d','--debug', help='enable debug mode', action='store_true')
     return parser.parse_args()
 
 
