bc256f7c1929becad94e7428c9b1fd8eeed2c7aa
michele-mada <michele.mm1234@gmail.com>
2017-06-28 17:44:08 +0200

    made cli parameters more tidy

--- py_eyetracker_v1.0/main.py
+++ py_eyetracker_v1.0/main.py
@@ -20,7 +20,6 @@ from utils.histogram.lsh_equalization import lsh_equalization
 from utils.bioID import BioIDFaceDatabase
 
 
-camera_port = 0
 
 algos = {
     "hough": PyHoughEyecenter,
@@ -117,7 +116,7 @@ def test_run(cli, algo):
 
 
 def live(cli, algo):
-    camera = WebcamVideoStream(src=camera_port,
+    camera = WebcamVideoStream(src=cli.camera_port,
                                debug=cli.debug,
                                contrast=None if cli.contrast == "None" else float(cli.contrast),
                                saturation=None if cli.saturation == "None" else float(cli.saturation)
@@ -152,7 +151,7 @@ def main(cli):
     algo.equalization = equaliz[cli.equalization]
 
     if cli.algo == "timm":
-        algo.context.load_program(cli.program_timm)
+        algo.context.load_program()
 
     if cli.file == "-":
         live(cli, algo)
@@ -164,23 +163,28 @@ def main(cli):
 
 def parsecli():
     parser = argparse.ArgumentParser(description="Find eyes and eye-centers from an image")
+    # main generic parameters
     parser.add_argument('file', help='filename of the picture; - for webcam; \"test\" to run a performance test', type=str)
+    parser.add_argument('-d', '--debug', help='enable debug mode', action='store_true')
+    # haar cascade detection parameters
     parser.add_argument('--eye-cascade-file', help='path to the .xml file with the eye-detection haar cascade',
                         type=str, default="../haarcascades/haarcascade_righteye_2splits.xml")
     parser.add_argument('--face-cascade-file', help='path to the .xml file with the face-detection haar cascade',
                         type=str, default="../haarcascades/haarcascade_frontalface_default.xml")
+    # camera parameters
     parser.add_argument('--saturation', help='override the webcam default saturation setting (value [0.0, 1.0])',
                         type=str, default="None")
     parser.add_argument('--contrast', help='override the webcam default contrast setting (value [0.0, 1.0])',
                         type=str, default="None")
+    parser.add_argument('-c','--camera-port', help='numeric index of the camera to use',
+                        type=int, default=0)
+    # algorithm selection parameters
     parser.add_argument('-a','--algo', help='pupil center algorithm to use',
                         type=str, default="timm", choices=algos.keys())
-    parser.add_argument('-p', '--program-timm', help='path to the opencl kernel implementing timm and barth algorithm',
-                        type=str, default="cl_kernels/timm_barth_kernel.cl")
     parser.add_argument('-e', '--equalization', help='type of histogram equalization to use',
                         type=str, default="h", choices=equaliz.keys())
-    parser.add_argument('-d','--debug', help='enable debug mode', action='store_true')
-    parser.add_argument('--bioid-folder', metavar='bioID folder', help='BioID face database folder',
+    # other
+    parser.add_argument('--bioid-folder', metavar='bioID_folder', help='BioID face database folder',
                         type=str, default="../../BioID-FaceDatabase-V1.2")
     return parser.parse_args()
 
